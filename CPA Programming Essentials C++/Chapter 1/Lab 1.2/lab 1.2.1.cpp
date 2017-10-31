4d07169437d9c65fd3b1f4d2db57b2cd36a7e648
PolulyakhSofiya <33236892+PolulyakhSofiya@users.noreply.github.com>
2017-10-31 12:41:35 +0200

    Update lab 1.2.1.cpp
    Delete empty rows

--- CPA Programming Essentials C++/Chapter 1/Lab 1.2/lab 1.2.1.cpp
+++ CPA Programming Essentials C++/Chapter 1/Lab 1.2/lab 1.2.1.cpp
@@ -1,20 +1,11 @@
 ﻿#include <iostream>
-
 using namespace std;
-
-
 
 int main(void) {
 
-
-
 	cout << "Hello, Sofiya! It's me, your first program.\n";
-
 //	cout << endl;
-
 	cout << "Hello, Liza! It's me, your first program.";
 
-
 	return 0;
-
 }
