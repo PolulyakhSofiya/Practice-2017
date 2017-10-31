9ab887b395a058f2dfa81697c1d04013f90f0ed1
PolulyakhSofiya <polulyakh.sofiya@gmail.com>
2017-10-31 12:38:19 +0200

    Chapter1 Lab 1.2.1

---
+++ CPA Programming Essentials C++/Chapter 1/Lab 1.2/lab 1.2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main(void) {
+
+
+
+	cout << "Hello, Sofiya! It's me, your first program.\n";
+
+//	cout << endl;
+
+	cout << "Hello, Liza! It's me, your first program.";
+
+
+	return 0;
+
+}
