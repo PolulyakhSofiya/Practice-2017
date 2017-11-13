81defc4835b3b345869d890920dc1023f46d4107
PolulyakhSofiya <polulyakh.sofiya@gmail.com>
2017-11-13 15:54:25 +0200

    some files of Chapter1

---
+++ CPA Programming Essentials C++/Chapter 1/Lab 1.3/lab 1.3.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+//start point of the program
+int main() {
+	
+	int minutes = 3;
+	int seconds = 60;
+	
+	cout << minutes << " minutes contain " << seconds << " seconds." << endl;
+	return 0;
+}
